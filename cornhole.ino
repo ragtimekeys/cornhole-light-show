f95c415859a69f024af2c7be3154d5e5f6d22cea
ragtimekeys <ragtimekeys@gmail.com>
2020-05-17 15:07:23 -0500

    Initial commit

---
+++ cornhole.ino
@@ -0,0 +1,12 @@
+#define BUTTON_PIN  2
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
